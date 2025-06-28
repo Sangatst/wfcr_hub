6456127d674e1199c47138df1b1d165951bff5bf
Dev_Code <Sangatst@users.noreply.github.com>
2025-06-28 21:21:27 +0600

    4th commit
    vite config changes

--- 10-days-forecast/vite.config.js
+++ 10-days-forecast/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/wfcr_hub/10-days-forecast/dist/',
 })
